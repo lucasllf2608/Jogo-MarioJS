72da1e780cecfd6f71fc620aa4606a6cb7fd875a
lucaslf <lucaslf@sme.fortaleza.ce.gov.br>
2023-07-19 12:24:05 -0300

    Alterando a função jump e colisão com o pipe

--- js/script.js
+++ js/script.js
@@ -12,11 +12,24 @@ const jump = () => {
 
 const loop = setInterval(()=>{
  const pipePosition = pipe.offsetLeft;
-  if (pipePosition <= 120){
+ const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', ' ');
+
+ 
+
+  if (pipePosition <= 120 && pipePosition > 0  && marioPosition < 100 ){
+    
     pipe.style.animation = 'none';
     pipe.style.left = `${pipePosition}px`;
+
+    mario.style.animation = 'none';
+    mario.style.bottom = `${marioPosition}px`;
+
+    console.log(marioPosition);
+
   }
 }, 10);
 
 document.addEventListener('keydown', jump);
 document.addEventListener("mousedown", jump);
+
+//https://www.youtube.com/watch?v=r9buAwVBDhA&list=PLzn85sQ8V2VP70sAuh18pd9AM5D4-Uli7&index=71&t=1727s
